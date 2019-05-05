367a50015b0679a4a13d156ba6ecab09d860fbd4
Glen Langer <contao.modul@glen-langer.de>
2019-05-06 00:50:04 +0200

    New language "fr"

---
+++ src/Resources/contao/languages/fr/tl_module.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/contao-botstatistics-bundle/language/fr/
+ *
+ * last-updated: 2019-05-06T00:45:55+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_module']['botstatistics_cron']['0'] = 'Supprimer automatiquement les anciennes données de statistiques?';
+$GLOBALS['TL_LANG']['tl_module']['botstatistics_cron']['1'] = 'Cochez la case pour activer la suppression des anciennes données statistiques (antérieures à 90 jours).';
+$GLOBALS['TL_LANG']['tl_module']['botstatistics_name']['0'] = 'Nom du module pour les statistiques';
+$GLOBALS['TL_LANG']['tl_module']['botstatistics_name']['1'] = 'Le nom est pour le choix des statistiques.';
+
