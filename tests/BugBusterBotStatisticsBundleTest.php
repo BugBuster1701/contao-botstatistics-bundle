4da7e3ea0b6dbf3f4ce976bed38af9ef59101e13
Glen Langer <contao.modul@glen-langer.de>
2023-12-21 11:45:05 +0100

    #16, simple unit test added

---
+++ tests/BugBusterBotStatisticsBundleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of a BugBuster Contao Bundle.
+ *
+ * @copyright  Glen Langer 2023 <http://contao.ninja>
+ * @author     Glen Langer (BugBuster)
+ * @package    Contao BotStatistics Bundle
+ * @link       https://github.com/BugBuster1701/contao-botstatistics-bundle
+ *
+ * @license    LGPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace BugBuster\BotStatisticsBundle\Tests;
+
+use BugBuster\BotStatisticsBundle\BugBusterBotStatisticsBundle;
+use PHPUnit\Framework\TestCase;
+
+class BugBusterBotStatisticsBundleTest extends TestCase
+{
+    public function testCanBeInstantiated(): void
+    {
+        $bundle = new BugBusterBotStatisticsBundle();
+
+        $this->assertInstanceOf('BugBuster\BotStatisticsBundle\BugBusterBotStatisticsBundle', $bundle);
+    }
+}
