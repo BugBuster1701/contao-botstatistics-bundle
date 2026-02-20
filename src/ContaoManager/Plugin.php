89ce262148f9faf14fa93c9078724164fb92f073
Glen Langer <contao.modul@glen-langer.de>
2026-02-20 13:00:16 +0100

    refactor(contao-manager): Add return type to getBundles and prepare for routing type

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -25,13 +25,19 @@ use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\RouteCollection;
 
 /**
  * Plugin for the Contao Manager.
  */
 class Plugin implements BundlePluginInterface, RoutingPluginInterface, ConfigPluginInterface
 {
-    public function getBundles(ParserInterface $parser)
+    /**
+     * Gets a list of autoload configurations for this bundle.
+     *
+     * @return array<ConfigInterface>
+     */
+    public function getBundles(ParserInterface $parser): array
     {
         return [
             BundleConfig::create('BugBuster\BotStatisticsBundle\BugBusterBotStatisticsBundle')
